adad18b385b0011b11a2fc7260dbdf46b7242577
xushuanghui <shuanghuixu@ShuangHuideMacBook-Pro.local>
2018-03-09 18:16:24 +0800

    1

--- Example/XshTimer/XshViewController.m
+++ Example/XshTimer/XshViewController.m
@@ -1,6 +1,7 @@
 
 #import "XshViewController.h"
 #import "MD_GCDTimerManager.h"
+#import "NSTimer+XshBlocksSupport.h"
 
 @interface XshViewController ()
 
@@ -13,12 +14,14 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    
+    
+    
+//    [[MD_GCDTimerManager sharedInstance] scheduledDispatchTimerWithName:@"xshTimer" timeInterval:3 queue:nil repeats:YES actionOption:(AbandonPreviousAction) action:^{
+//        NSLog(@"1234567");
+//    }];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
